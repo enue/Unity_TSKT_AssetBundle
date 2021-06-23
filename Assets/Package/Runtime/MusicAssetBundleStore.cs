6b9b1ee67a227ceaf21a029bd3b237f2d6811c3c
enu <otoriden@gmail.com>
2021-06-23 21:08:03 +0900

    create project

---
+++ Assets/Package/Runtime/MusicAssetBundleStore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Cysharp.Threading.Tasks;
+#nullable enable
+
+#if TSKT_ASSETBUNDLE_SUPPORT_SOUND
+namespace TSKT
+{
+    public class MusicAssetBundleStore : IMusicStore
+    {
+        Music[] musics = System.Array.Empty<Music>();
+
+        public async void Add(string filename, string filepath, int priority)
+        {
+            var loader = new LocalAssetBundleLoader();
+            var loadeds = await loader.LoadAllAsync<Music>(filename, filepath, priority: priority);
+            if (loadeds.Succeeded)
+            {
+                musics = musics.Concat(loadeds.value).ToArray();
+            }
+            UnityEngine.Assertions.Assert.IsTrue(loadeds.Succeeded, "failed loading music assetbundle. " + loadeds.exception?.ToString());
+        }
+
+        public Music? Get(string musicName)
+        {
+            foreach (var it in musics)
+            {
+                if (it.name == musicName)
+                {
+                    return it;
+                }
+            }
+            Debug.Assert(musics != null, "music store is not loaded yet");
+            return null;
+        }
+    }
+}
+#endif
